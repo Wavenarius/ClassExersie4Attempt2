6ea1c0aa6e041fddcecd91a49110da88907a69a1
wml5@students.ptcollege.edu <>
2018-10-24 15:48:37 -0400

    Take this

---
+++ src/classexercise4/ClassExercise4.java
@@ -0,0 +1,16 @@
+/*
+ * wml5
+ * 10/24/2018
+ * Class Exercise 4
+ */
+package classexercise4;
+
+public class ClassExercise4 
+{
+    
+ static void main(String[] args) 
+    {
+        System.out.println("(*/^*)/     _|_____|_");
+    }
+    
+}
